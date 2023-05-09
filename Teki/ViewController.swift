53eefed8626f8a90f3b740f57be11ba5221c71aa
Clara <delianov.clara@gmail.com>
2023-05-09 15:46:10 +0200

    interface mise en placevia storyboard

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quoteLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
